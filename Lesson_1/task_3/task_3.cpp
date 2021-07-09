ee98c1f07c56cad8c10c892390f61d447a983efe
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-09 03:06:40 +0300

    Update task_3.cpp
    Change 1

--- Lesson_1/task_3/task_3.cpp
+++ Lesson_1/task_3/task_3.cpp
@@ -1,21 +1,21 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int oreshki__N = 0;
-	int shishki__M = 0;
-	int oresh_v_shish__K = 0;
+	int N = 0;
+	int M = 0;
+	int K = 0;
 
 	cout << "Skolko nuzhno oreshkov: ";
-	cin >> oreshki__N;
+	cin >> N;
 	cout << "Skolko sobrala shishok: ";
-	cin >> shishki__M;
+	cin >> M;
 	cout << "Skolko oreshkov v shishke: ";
-	cin >> oresh_v_shish__K;
+	cin >> K;
 
-	if ((shishki__M * oresh_v_shish__K) >= oreshki__N)
+	if ((M * K) >= N)
 	{
 		cout << "OK";
 	}
