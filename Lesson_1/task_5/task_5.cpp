4a6e2537317246ba39fafe32942a20fa90ae7df8
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-10 19:50:43 +0300

    Update task_5.cpp
    Change 5

--- Lesson_1/task_5/task_5.cpp
+++ Lesson_1/task_5/task_5.cpp
@@ -4,18 +4,28 @@ using namespace std;
 
 int main()
 {
-    int pervoye_chislo = 0;
-    int vtoroye_chislo = 0;
-    int deistvie = 0;
+    double pervoye_chislo = 0;
+    double vtoroye_chislo = 0;
+    short deistvie = 0;
+
+    cout << "Dodro pozalovat v kalkulyator!" << endl;
 
     cout << "Vvedite pervoe chislo: ";
     cin >> pervoye_chislo;
     cout << "Vvedite vtoroe chislo: ";
     cin >> vtoroye_chislo;
-    cout << "Vvedite deistvie: ";
+    cout << endl << "Vvedite deistvie. " << endl << endl
+        << "1 - dobavit" << endl 
+        << "2 - otnyat" << endl 
+        << "3 - umnozhit" << endl 
+        << "4 - podelit" << endl << endl;
+    cout << "Vash vibor: ";
     cin >> deistvie;
 
-    cout << "Resultat: ";
+    if (deistvie > 0 && deistvie < 5)
+    {
+        cout << endl << "Resultat: ";
+    }
 
     switch (deistvie)
     {
@@ -32,6 +42,9 @@ int main()
             cout << pervoye_chislo / vtoroye_chislo;
            break;
         default:
-            cout << "nevernoye deystviye!";
+            cout << endl << "Sluchilas oshibka!"<< endl 
+                <<"Vi vveli nevernoye chislo!"<< endl
+                <<"Poprobuyte yescho raz :)";
     }
+    cout << endl << endl << "Spasibo chto vi polzuetes nashey programmoy! :)" << endl << endl;
 }
