935985dc1c2a0856d9ced62a932d426183b78d41
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-13 15:15:32 +0300

    Change 1
    Added 4 tasks of the second lesson.

---
+++ lesson_2/task_2/task_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "Welcome!" << endl;
+    cout << "Enter number: ";
+    cin >> number;
+
+    cout << endl <<"Numbers: ";
+
+    for (int i = 0; i <= number; i++)
+    {
+        cout << i << ", ";
+    }
+
+    cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
+
