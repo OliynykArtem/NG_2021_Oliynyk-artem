66ed864cd41ebc89b6a4ae0af69eb49e458dd257
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-20 19:36:11 +0300

    added the first task of the third lesson

---
+++ Lesson_3/task_1/task_1/task_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Welcome!" << endl;
+	cout << "please enter the numbers of ten schools" << endl << endl;
+
+	int school_numbers[10];
+	int our_school = 0;
+	string message;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << i + 1 << ".school Number: ";
+		cin >> school_numbers[i];
+	}
+
+	cout << endl << "Please enter your school number: ";
+	cin >> our_school;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (our_school == school_numbers[i])
+		{
+			message = "I know this school";
+			break;
+		}
+		else
+		{
+			message = "I don't know this school";
+		}
+	}
+	cout << endl << message << endl;
+
+	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
