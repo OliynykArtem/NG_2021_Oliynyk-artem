f01cdb1fafee296014a739cf1a185a621e93aac9
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-23 17:02:40 +0300

    deleted unneeded folders

---
+++ lesson_2/task_5/task_5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size = 0;
+
+	cout << "Welcome!" << endl;
+	cout << "Christmas tree size: ";
+	cin >> size;
+
+	cout << endl;
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = size; j > i; j--)
+		{
+			cout << " ";
+		}
+		for (int k = 0; k <= i * 2; k++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << " ";
+	}
+
+	cout << "*";
+
+	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
+
