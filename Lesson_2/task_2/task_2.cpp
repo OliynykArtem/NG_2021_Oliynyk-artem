2f5b944d2395bda12d03a6a8f37703512dfab84b
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-24 10:01:19 +0300

    added Lesson 2

---
+++ Lesson_2/task_2/task_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+
+    cout << "Welcome!" << endl;
+    cout << "Enter number: ";
+    cin >> number;
+
+    cout << endl <<"Numbers: ";
+
+    for (int i = 0; i <= number; i++)
+    {
+        cout << i;
+        i < number ? cout << ", " : cout << ".";
+    }
+
+    cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
+
---
+++ Lesson_2/task_3/task_3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Square_size = 0;
+
+    cout << "Welcome" << endl;
+    cout << "Square size: ";
+    cin >> Square_size;
+
+    cout << endl;
+
+    for (int i = 0; i < Square_size; i++)
+    {
+        for (int j = 0; j < Square_size; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
