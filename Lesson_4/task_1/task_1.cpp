cac6161b641a11b5c1e7c3ac2d386939a34fee18
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-07-29 17:02:41 +0300

    added task 1

---
+++ Lesson_4/task_1/task_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char arr[1000];
+	int words = 0;
+	int size = 0;
+
+	cout << "Welcome!" << endl;
+	cout << "Please enter any line: ";
+	cin.getline(arr, 1000);
+
+	for (int i = 0; arr[i] != 0; i++)
+	{
+		if (!((arr[i] >= 65 && arr[i] <= 90) || (arr[i] >= 97 && arr[i] <= 122)))
+		{
+			arr[i] = 0;
+		}
+		size++;
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] != 0 && arr[i + 1] == 0)
+		{
+			words++;
+		}
+	}
+
+	cout << "Result - " << words << endl;
+	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
