0f265a993a53f8335ab1585890090c6e12b4823d
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-08-04 15:33:03 +0300

    Change 1 in task 2 of lesson 4

--- Lesson_4/task_2/task_2.cpp
+++ Lesson_4/task_2/task_2.cpp
@@ -10,74 +10,47 @@ int main()
 	int words = 0;
 	int size = 0;
 	int max = 0;
-	int word_num = 0;
+	int first_symbol = 0;
+	int max_first_symbol = 0;
 
-	// Сообщение пользователю
+	// Message
 	cout << "Welcome!" << endl;
 	cout << "Please enter any line: ";
 	cin.getline(inj_arr, 1000);
 	
-
-	for (int i = 0; inj_arr[i] != 0; i++)
-	{
-		if (((inj_arr[i] >= 'A' && inj_arr[i] <= 'Z') || (inj_arr[i] >= 'a' && inj_arr[i] <= 'z')))
-		{
-			if (!((inj_arr[i + 1] >= 'A' && inj_arr[i + 1] <= 'Z') || (inj_arr[i + 1] >= 'a' && inj_arr[i + 1] <= 'z')))
-			{
-				words++;
-			}
-
-		}
-
-	}
-
-	// Считаем сколько символов в каждом слове
 	int j = 0;
-	for (int i = 0; i < words; i++)
+	for (int i = 0; inj_arr[i - 1] != 0; i++)
 	{
-		for (int k = 0; ; j++)
+		if (((inj_arr[i] >= 'A' && inj_arr[i] <= 'Z') || 
+			(inj_arr[i] >= 'a' && inj_arr[i] <= 'z')))
 		{
-			if (inj_arr[j] == 0 && inj_arr[j + 1] != 0)
-			{
-				j++;
-				break;
-			}
+			size++;
 
-			if (inj_arr[j] != 0)
+			if (j == 0)
 			{
-				word_len[i] += 1;
-
-				// Запоминаем место первого символа каждого слова
-				if (k == 0)
-				{
-					firsts_symbols[i] = j;
-					k = 1;
-				}
+				first_symbol = i;
+				j = 1;
 			}
-			else
-			{
-				continue;
-			}
+			
 		}
-	}
-	
-	// Находим самое большое слово
-	max = word_len[0];
-	for (int i = 0; i < words; i++)
-	{
-		if (max < word_len[i])
+		else
 		{
-			max = word_len[i];
-			word_num = i;
+			if (size > max)
+			{
+				max_first_symbol = first_symbol;
+				max = size;
+			}
+			size = 0;
+			j = 0;
 		}
 	}
 
-	// Выводим слово на консоль
-	for (int i = firsts_symbols[word_num]; inj_arr[i] != 0; i++)
+	for (int i = max_first_symbol; i < max_first_symbol + max; i++)
 	{
 		cout << inj_arr[i];
 	}
 
-	// Сообщение пользователю
+
+	// Message
 	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
 }
