4733f64b4c62aeee20e8b7ec5d074eab291e108a
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-08-04 16:16:05 +0300

    added lesson 5

---
+++ Lesson_5/task_1/task_1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int set_size()
+{
+	int size = 0;
+	cout << "Welcome!" << endl;
+	cout << "Christmas tree size: ";
+	cin >> size;
+
+	return size;
+}
+
+int tree_output(int size)
+{
+	cout << endl;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = size; j > i; j--)
+		{
+			cout << " ";
+		}
+		for (int k = 0; k <= i * 2; k++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return size;
+}
+
+void tree_trunk_output(int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << " ";
+	}
+
+	cout << "*";
+}
+
+int main()
+{
+	tree_trunk_output(tree_output(set_size()));
+	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
