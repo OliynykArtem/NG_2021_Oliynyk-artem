41579c572d030b48d3191343cd70416974d98942
Artem Oliynyk <79795767+OliynykArtem@users.noreply.github.com>
2021-08-06 22:55:38 +0300

    change 1 in task 3 of lesson 5

--- Lesson_5/task_3/task_3.cpp
+++ Lesson_5/task_3/task_3.cpp
@@ -2,36 +2,40 @@
 
 using namespace std;
 
-int count_words(char arr[])
-{
-	int words = 0;
-	for (int i = 0; arr[i] != 0; i++)
-	{
-		if (((arr[i] >= 'A' && arr[i] <= 'Z') || (arr[i] >= 'a' && arr[i] <= 'z')) &&
-			(!((arr[i + 1] >= 'A' && arr[i + 1] <= 'Z') || (arr[i + 1] >= 'a' && arr[i + 1] <= 'z'))))
-		{
-			words++;
-		}
-	}
-	return words;
-}
-
-void data_input(char arr[])
+void string_input(char arr[])
 {
 	cout << "Welcome!" << endl;
 	cout << "Please enter any line: ";
 	cin.getline(arr, 1000);
 }
 
-void data_output(char arr[])
+void result(int words)
 {
-	cout << "Result - " << count_words(arr) << endl;
+	cout << "Result - " << words << endl;
 	cout << endl << endl << "Thanks for using our program :)" << endl << endl;
+}
+
+int count_words_2(char i, char j, int words)
+{
+	if (((i >= 'A' && i <= 'Z') || (i >= 'a' && i <= 'z')) &&
+		(!((j >= 'A' && j <= 'Z') || (j >= 'a' && j <= 'z'))))
+		words++;
+
+	return words;
+}
+
+void count_words(char arr[])
+{
+	int words = 0;
+	for (int i = 0; arr[i] != 0; i++)
+		words = count_words_2(arr[i], arr[i + 1], words);
+
+	result(words);
 }
 
 int main()
 {
 	char arr[1000];
-	data_input(arr);
-	data_output(arr);
+	string_input(arr);
+	count_words(arr);
 }
